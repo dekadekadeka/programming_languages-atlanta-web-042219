f12e664cf5856dfa3f3d529c0e6685ec5acc1c1d
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-12 04:27:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def reformat_languages(languages)
+  # languages.map{|language| binding.pry}
+
+  new_hash = {}
+  style_hash = {}
+  new_style = {}
   # your code here
+  languages.each do |style, language|
+#    binding.pry
+   style_hash[:style] = languages.keys
+   if style_hash[:style].length == 2
+     new_style[:style] ||= []
+      new_style[:style] << [style]
+    else
+      new_style[:style] = [style]
+   end
+
+     language.each do |lang, type|
+#       binding.pry
+      new_hash[lang] = type.merge(new_style)
+    end
+  end
+  new_hash
 end
