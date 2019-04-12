d429872fa040d725c1060755944d78e12c311973
Renee Deka Ambia <dekadekadeka@gmail.com>
2019-04-12 04:34:33 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def reformat_languages(languages)
+  new_hash = {}
+  # style_hash = {}
+  # new_style = {}
   # your code here
+  languages.each do |style, language|
+  #  style_hash[:style] = languages.keys
+  #  if style_hash[:style].length == 2
+  #    new_style[:style] ||= []
+  #     new_style[:style] << [style]
+  #   else
+  #     new_style[:style] = [style]
+  #  end
+     language.each do |lang, type|
+       new_hash[lang] ||= type
+       new_hash[lang][:style] ||= []
+       new_hash[lang][:style] << style
+#      new_hash[lang] = type.merge(new_style)
+    end
+  end
+  new_hash
 end
